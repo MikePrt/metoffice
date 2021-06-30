3fdb8616eaa6d1b393bce6f18eea581847883799
MikePrt <mikeparrott2008@gmail.com>
2021-06-30 07:43:59 +0100

    updatee of checking routines

--- obs_analysis.R
+++ obs_analysis.R
@@ -4,17 +4,23 @@ library(rvest)
 
 all_obs <- read_csv("data/all_obs.csv",  col_types = "Tcccccccc", na = c("", "NA", "-"))
 
+range(all_obs$obs_at)
 
 all_obs %>% select(obs_at) %>% mutate(delta_hr = c(NA, diff(obs_at))) %>% 
   filter(delta_hr != 1)
 
 
 
-all_obs %>% mutate(temp = parse_number(temp)) %>% 
+all_obs <- all_obs %>% mutate(temp = parse_number(temp),
+                              rain = weather %in% c("Drizzle", "Heavy rain" , "Light rain"))
+
+all_obs %>% summarise(pct_rain = mean(rain))
+
+all_obs %>% 
   ggplot(aes(x = obs_at, y = temp)) + geom_line()
 
 
-#all_obs %>%  count(weather) %>% pull(weather)
+all_obs %>%  count(weather) %>% pull(weather)
 
 weather_lookup <- tibble(weather = c("Cloudy" ,"Drizzle", "Fog" , "Heavy rain" , "Light rain" , "Mist"  ,         
                                       "Overcast" ,  "Sunny day" , "Sunny intervals"),
